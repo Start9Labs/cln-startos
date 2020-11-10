4bc98e93f5431c49ee83341dee4e3825d94064ea
Aiden McClelland <me@drbonez.dev>
2020-11-10 11:10:37 -0700

    do ln in bash

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,4 +3,6 @@
 export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 
 configurator
+ls -d /root/.lightning/shared/*/lightning-rpc | xargs rm
+ls -d /root/.lightning/shared/* | xargs ln /root/.lightning/bitcoin/lightning-rpc
 exec tini lightningd
