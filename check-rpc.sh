20edd8a745896e3fdae36942582a19a3be5da43e
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-02-20 23:46:19 -0500

    fix health check for eos parsing warning

--- check-rpc.sh
+++ check-rpc.sh
@@ -4,9 +4,9 @@ DURATION=$(</dev/stdin)
 if (($DURATION <= 30000 )); then
     exit 60
 else
-    lightning-cli getinfo
-    RES=$?
-    if test "$RES" != 0; then
+    lightning-cli getinfo &>/dev/null
+    exit_code=$?
+    if test "$exit_code" != 0; then
         echo "RPC interface is unreachable" >&2
         exit 1
     fi
