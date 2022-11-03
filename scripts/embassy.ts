0565dbbeae590cdaad6489516c6e4978a38af7ae
Chris Guida <chrisguida@gmail.com>
2022-11-03 14:00:31 -0600

    fix main and remove wait for stdin in hck

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -3,4 +3,5 @@ export { dependencies } from "./procedures/dependencies.ts";
 export { properties } from "./procedures/properties.ts";
 export { getConfig } from "./procedures/getConfig.ts";
 export { migration } from "./procedures/migrations.ts";
-export { health } from "./procedures/healthChecks.ts";
+export { health } from "./procedures/health.ts";
+export { main } from "./procedures/main.ts";
