14583381a666a3711000648783a1dbb6ca542f62
Chris Guida <chrisguida@gmail.com>
2022-11-03 13:14:13 -0600

    convert main and hcks to js
    fix main and remove wait for stdin in hck

--- check-rpc.sh
+++ check-rpc.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-DURATION=$(</dev/stdin)
-if (($DURATION <= 30000 )); then
-    exit 60
-else
-    lightning-cli getinfo &>/dev/null
-    exit_code=$?
-    if test "$exit_code" != 0; then
-        echo "RPC interface is unreachable" >&2
-        exit 1
-    fi
+lightning-cli getinfo &>/dev/null
+exit_code=$?
+if test "$exit_code" != 0; then
+    echo "RPC interface is unreachable" >&2
+    exit 1
 fi
