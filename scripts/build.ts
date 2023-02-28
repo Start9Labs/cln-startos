a978174656709a82fcea9bba2ec366c1d253c3ad
BluJ <mogulslayer@gmail.com>
2023-02-28 13:11:35 -0700

    chore: Revert back to bundle

--- scripts/build.ts
+++
@@ -1,9 +0,0 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.17.10/mod.js";
-await esbuild.build({
-  bundle: true,
-  outfile: "./scripts/embassy.js",
-  entryPoints: ["./scripts/embassy.ts"],
-  format: "esm",
-  target: "es2020",
-});
-esbuild.stop();
