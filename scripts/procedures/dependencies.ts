dd05899c99e6c45a41fb498330142905c198fd71
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-07-10 17:56:29 -0400

    test bitcoind check with logs and remove autoconfigure

--- scripts/procedures/dependencies.ts
+++ scripts/procedures/dependencies.ts
@@ -28,24 +28,24 @@ export const dependencies: T.ExpectedExports.dependencies = {
     // deno-lint-ignore require-await
     async check(effects, configInput) {
       effects.info("check bitcoind");
-      if (matchOldBitcoindConfig.test(configInput) && configInput.advanced.pruning.mode !== "disabled") {
-        return {
-          error:
-            'Pruning must be disabled to use CLN with <= 24.0.1 of Bitcoin Core. To use CLN with a pruned node, update Bitcoin Core to >= 25.0.0~2.',
-        };
+      if (matchOldBitcoindConfig.test(configInput)) {
+        effects.info('bitcoin core config <=24.0.1')
+        if (configInput.advanced.pruning.mode !== "disabled"){
+          const error = 'Pruning must be disabled to use CLN with <= 24.0.1 of Bitcoin Core. To use CLN with a pruned node, update Bitcoin Core to >= 25.0.0~2.'
+          effects.error(`throwing error: ${error}`);
+          return {
+            error
+          }
+        }
+        effects.info('node is not pruned')
+        return { result: null }
       }
       return { result: null };
     },
     // deno-lint-ignore require-await
     async autoConfigure(effects, configInput) {
       effects.info("autoconfigure bitcoind");
-      if (matchOldBitcoindConfig.test(configInput)) {
-        configInput.advanced.pruning.mode = "disabled"
-        return { result: configInput}
-      } else {
-        const config = matchBitcoindConfig.unsafeCast(configInput);
-        return { result: config };
-      }
+      return { result: configInput };
     },
   },
 };
