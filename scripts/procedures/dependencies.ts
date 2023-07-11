457cd092300fa1b8ee5323e6b8ed45f3a9fadcb7
Dominion5254 <125493428+Dominion5254@users.noreply.github.com>
2023-07-11 10:18:01 -0600

    fix dependency error and autoconfig (#92)
    * fix dependency error and autoconfig
    
    * test bitcoind check with logs and remove autoconfigure
    
    * revert autoconfigure and remove debug error logging
    
    ---------
    
    Co-authored-by: Lucy Cifferello <12953208+elvece@users.noreply.github.com>

--- scripts/procedures/dependencies.ts
+++ scripts/procedures/dependencies.ts
@@ -2,6 +2,19 @@ import { matches, types as T } from "../deps.ts";
 
 const { shape, string } = matches;
 
+const matchOldBitcoindConfig = shape({
+  rpc: shape({
+    advanced: shape({
+      serialversion: matches.any
+    }),
+  }),
+  advanced: shape({
+    pruning: shape({
+      mode: string,
+    }),
+  }),
+})
+
 const matchBitcoindConfig = shape({
   advanced: shape({
     pruning: shape({
@@ -15,13 +28,24 @@ export const dependencies: T.ExpectedExports.dependencies = {
     // deno-lint-ignore require-await
     async check(effects, configInput) {
       effects.info("check bitcoind");
-      return { result: null };
-    },
+      if (matchOldBitcoindConfig.test(configInput) && configInput.advanced.pruning.mode !== "disabled") {
+        return {
+          error:
+            'Pruning must be disabled to use CLN with <= 24.0.1 of Bitcoin Core. To use CLN with a pruned node, update Bitcoin Core to >= 25.0.0~2.',
+        };
+      }
+        return { result: null };
+      },
     // deno-lint-ignore require-await
     async autoConfigure(effects, configInput) {
       effects.info("autoconfigure bitcoind");
-      const config = matchBitcoindConfig.unsafeCast(configInput);
-      return { result: config };
+      if (matchOldBitcoindConfig.test(configInput)) {
+        configInput.advanced.pruning.mode = "disabled"
+        return { result: configInput}
+      } else {
+        const config = matchBitcoindConfig.unsafeCast(configInput);
+        return { result: config };
+      }
     },
   },
 };
