cdefa5a2b5ab425f7ebabaa33f533a2c0b939106
Shadowy Super Coder <musashidisciple@proton.me>
2023-09-12 14:04:52 -0600

    Add generate-rune action

---
+++ actions/generate-rune.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+json_output="$(lightning-cli commando-rune)"
+RUNE="$(echo "$json_output" | jq -r '.rune')"
+
+action_result="    {
+    \"version\": \"0\",
+    \"message\": \"Successfully Added Rune\",
+    \"value\": $RUNE,
+    \"copyable\": true,
+    \"qr\": true
+    \"masked\": true
+}"
+
+echo $action_result
