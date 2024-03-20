36901d303edf487e749cb1adbbfa668a29ffa2fe
ShadowySuperCoder <musashidisciple@proton.me>
2024-03-19 20:52:40 -0600

    complete rescan-blockchain action

--- actions/rescan-blockchain.sh
+++ actions/rescan-blockchain.sh
@@ -7,19 +7,32 @@ BLOCKHEIGHT_OR_DEPTH=$(jq -r '.["blockheight-or-depth"]' input.json)
 rm input.json
 echo "$BLOCKHEIGHT_OR_DEPTH" > /root/.lightning/rescan.txt
 
+if [ $BLOCKHEIGHT_OR_DEPTH -lt 0 ]; then
+    BLOCKHEIGHT=$(( -BLOCKHEIGHT_OR_DEPTH ))
+    SCAN_MESSAGE="from blockheight $BLOCKHEIGHT"
+else
+    SCAN_MESSAGE="$BLOCKHEIGHT_OR_DEPTH blocks from the tip"
+fi
+
 action_result_running="    {
     \"version\": \"0\",
-    \"message\": \"Core Lightning restarting and scanning blocks from the specified blockheight or depth\",
+    \"message\": \"Core Lightning restarting and rescanning $SCAN_MESSAGE\",
     \"value\": null,
     \"copyable\": false,
     \"qr\": false
 }"
 action_result_stopped="    {
     \"version\": \"0\",
-    \"message\": \"Core Lightning will rescan blocks from the specified blockheight or depth next time the service is started\",
+    \"message\": \"On the next service start Core Lightning will rescan $SCAN_MESSAGE\",
     \"value\": null,
     \"copyable\": false,
     \"qr\": false
 }"
 
-lightning-cli stop >/dev/null 2>/dev/null && echo $action_result_running || echo $action_result_stopped
+lightning-cli getinfo >/dev/null 2>/dev/null && EXIT_CODE=$? || EXIT_CODE=$?
+
+if [ $EXIT_CODE -eq 0 ]; then
+    lightning-cli stop >/dev/null 2>/dev/null || echo $action_result_running
+else
+    echo $action_result_stopped
+fi
