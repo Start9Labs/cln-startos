8554c68c0da98d6915bd18a860ce3bbb9c1c88d7
Derek Smith <derek@farscapian.com>
2024-03-23 15:07:59 -0400

    Add Clams Remote websocket properties.

--- scripts/procedures/properties.ts
+++ scripts/procedures/properties.ts
@@ -89,6 +89,14 @@ export const properties: T.ExpectedExports.properties = async (
   if (
     (await util.exists(effects, {
       volumeId: "main",
+      path: "start9/clamsRemoteWebsocketTorAddress",
+    })) === false
+  ) {
+    return noPropertiesFound;
+  }
+  if (
+    (await util.exists(effects, {
+      volumeId: "main",
       path: "start9/watchtowerTorAddress",
     })) === false
   ) {
@@ -113,6 +121,12 @@ export const properties: T.ExpectedExports.properties = async (
       path: "start9/restTorAddress",
     })
     .then((x) => x.trim());
+  const clamsRemoteWebsocketTorAddress = await effects
+    .readFile({
+      volumeId: "main",
+      path: "start9/clamsRemoteWebsocketTorAddress",
+    })
+    .then((x) => x.trim());
   const watchtowerTorAddress = await effects
     .readFile({
       volumeId: "main",
@@ -133,6 +147,26 @@ export const properties: T.ExpectedExports.properties = async (
       volumeId: "main",
     })
   );
+  const websocketProperties: T.PackagePropertiesV2 = !config.advanced.clams_remote_websocket
+    ? {}
+    : {
+      "Clams Remote Websocket Onion": {
+        type: "string",
+        value: `${clamsRemoteWebsocketTorAddress}`,
+        description: "The onion endpoint for your CLN websocket service for use with Clams Remote.",
+        copyable: true,
+        qr: false,
+        masked: true,
+      },
+      "Clams Remote Websocket Port": {
+        type: "string",
+        value: "7272",
+        description: "The service endpoint for your CLN websocket service for use with Clams Remote.",
+        copyable: true,
+        qr: false,
+        masked: false,
+      }
+    };
 
   const restProperties: T.PackagePropertiesV2 = !config.advanced.plugins.rest
     ? {}
@@ -335,6 +369,15 @@ export const properties: T.ExpectedExports.properties = async (
         value: `${nodeInfo.id}@${peerTorAddress}`,
         description:
           "Share this URI with others so they can add your CLN node as a peer",
+        copyable: true,
+        qr: true,
+        masked: true,
+      },
+      "Clams Remote Websocket URI": {
+        type: "string",
+        value: `${nodeInfo.id}@${clamsRemoteWebsocketTorAddress}:7272`,
+        description:
+          "The URI needed by Clams Remote to connect to Core Lightning's websocket interface.",
         copyable: true,
         qr: true,
         masked: true,
