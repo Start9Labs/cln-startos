9ad0332f5b2f0c19da8e2754b8091adbd3860bbb
Shadowy Super Coder <musashidisciple@proton.me>
2024-09-08 12:13:21 -0600

    improve action_result message

--- actions/delete-gossip.sh
+++ actions/delete-gossip.sh
@@ -4,14 +4,14 @@ set -e
 
 action_result_running="    {
     \"version\": \"0\",
-    \"message\": \"gossip_store has been deleted. Core Lightning is restarting\",
+    \"message\": \"The gossip_store has been deleted. Core Lightning is restarting\",
     \"value\": null,
     \"copyable\": false,
     \"qr\": false
 }"
 action_result_stopped="    {
     \"version\": \"0\",
-    \"message\": \"On the next service start Core Lightning will rebuild gossip_store from peers\",
+    \"message\": \"The gossip_store has been deleted. On the next service start Core Lightning will rebuild gossip_store from peers\",
     \"value\": null,
     \"copyable\": false,
     \"qr\": false
