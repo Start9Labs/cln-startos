35decdd9ab0685ddbee9864066e47324fbe3d692
Shadowy Super Coder <musashidisciple@proton.me>
2024-11-25 13:38:08 -0700

    export createBackup

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -3,3 +3,4 @@ export { dependencies } from "./procedures/dependencies.ts";
 export { properties } from "./procedures/properties.ts";
 export { getConfig } from "./procedures/getConfig.ts";
 export { migration } from "./procedures/migrations.ts";
+export { createBackup } from "./procedures/backups.ts"
