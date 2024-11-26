9740eb3c32674c40d3fc5cdbbbbebe1acbdb6de5
Shadowy Super Coder <musashidisciple@proton.me>
2024-11-25 18:20:16 -0700

    remove exclusion

--- scripts/procedures/backups.ts
+++ scripts/procedures/backups.ts
@@ -1,7 +1,7 @@
 import { Backups } from "../deps.ts";
 
 export const { createBackup } = Backups.with_options({
-  exclude: [".lightning/bitcoin/lightningd.sqlite3"],
+  // exclude: [".lightning/bitcoin/lightningd.sqlite3"],
 })
   .volumes("main", "cert")
   .build();
