d30fa5f1d41bff3e27704ffe20b704e7a6051206
Shadowy Super Coder <musashidisciple@proton.me>
2024-11-25 18:35:46 -0700

    only backup main

--- scripts/procedures/backups.ts
+++ scripts/procedures/backups.ts
@@ -3,5 +3,5 @@ import { Backups } from "../deps.ts";
 export const { createBackup } = Backups.with_options({
   // exclude: [".lightning/bitcoin/lightningd.sqlite3"],
 })
-  .volumes("main", "cert")
+  .volumes("main")
   .build();
