7847ab9e96f23fc2a33b41740bc34ee06e3ac84f
Shadowy Super Coder <musashidisciple@proton.me>
2024-12-16 08:53:22 -0700

    revert backup create to the entrypoint

--- assets/compat/backup-create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-touch /root/.lightning/.backupignore
-echo "/root/.lightning/bitcoin/lightningd.sqlite3" > /root/.lightning/.backupignore
-compat duplicity create /mnt/backup /root/.lightning
