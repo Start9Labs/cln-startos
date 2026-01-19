3b6ff2c22027d782578570942b050dfcde9573f0
Shadowy Super Coder <musashidisciple@proton.me>
2026-01-19 10:31:04 -0700

    bump pre-release and disallow downgrades

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -486,8 +486,20 @@ export const migration: T.ExpectedExports.migration =
           throw new Error("Cannot downgrade");
         },
       },
+      "25.12.1.1": {
+        up: compat.migrations.updateConfig(
+          (config) => {
+            return config;
+          },
+          true,
+          { version: "25.12.1.1", type: "up"},
+        ),
+        down: () => {
+          throw new Error("Cannot downgrade");
+        },
+      },
     },
-    "25.12.1",
+    "25.12.1.1",
   );
 
 function generateRandomString(length: number) {
