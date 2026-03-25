e7ee07fe089ca4d07a3512dc9ea381c263dc2008
Matt Hill <mattnine@protonmail.com>
2026-03-25 17:14:31 -0600

    ignore files for backup

--- startos/backups.ts
+++ startos/backups.ts
@@ -5,7 +5,14 @@ export const { createBackup, restoreInit } = sdk.setupBackups(
   async ({ effects }) =>
     sdk.Backups.ofVolumes('main')
       .setOptions({
-        exclude: ['bitcoin/lightning-rpc', 'bitcoin/lightningd.sqlite3'],
+        exclude: [
+          'bitcoin/lightning-rpc',
+          'bitcoin/lightningd.sqlite3',
+          'bitcoin/lightningd.sqlite3-wal',
+          'bitcoin/lightningd.sqlite3-shm',
+          'bitcoin/gossip_store',
+          'data/app/application-cln.log',
+        ],
       })
       .setPostRestore(async (effects) => {
         await storeJson.merge(effects, { restore: true })
