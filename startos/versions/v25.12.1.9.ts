0854ead7d4d378c3ec1d50b8d2c2e7c0672d2692
Aiden McClelland <me@drbonez.dev>
2026-04-28 23:06:25 -0600

    Bump version to 25.12.1:9

---
+++ startos/versions/v25.12.1.9.ts
@@ -0,0 +1,16 @@
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+
+export const v_25_12_1_9 = VersionInfo.of({
+  version: '25.12.1:9',
+  releaseNotes: {
+    en_US: 'Internal updates (start-sdk 1.3.3)',
+    es_ES: 'Actualizaciones internas (start-sdk 1.3.3)',
+    de_DE: 'Interne Aktualisierungen (start-sdk 1.3.3)',
+    pl_PL: 'Aktualizacje wewnętrzne (start-sdk 1.3.3)',
+    fr_FR: 'Mises à jour internes (start-sdk 1.3.3)',
+  },
+  migrations: {
+    up: async ({ effects }) => {},
+    down: IMPOSSIBLE,
+  },
+})
